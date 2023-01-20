1e15b84cf595f19676bdb0b81e2bb014c9f11a09
Daniel Oyati <111546438+DanielOyati@users.noreply.github.com>
2023-01-20 11:53:56 +0100

    updated javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // set current year
-const yearEl = document.querySelector(".year");
+const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
@@ -8,87 +8,89 @@ console.log(currentYear);
 ////////////////////  /////////////////////////
 // make mobile navigation work
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
 });
 
 ///////////////////////////////////////////////// smooth acrolling animation
 
-const allLinks = document.querySelectorAll("a:link");
+const allLinks = document.querySelectorAll('a:link');
 // console.log(allLinks);
 
 allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
+  link.addEventListener('click', function (e) {
     // console.log(e);
     e.preventDefault();
-    const href = link.getAttribute("href");
+    const href = link.getAttribute('href');
     console.log(href);
 
     // scroll back to the top
-    if (href === "#")
+    if (href === '#')
       window.scrollTo({
         top: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
 
     // scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
+    if (href !== '#' && href.startsWith('#')) {
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
     }
 
     // close mobile navigation
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
   });
 });
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 
-const sectionHeroEl = document.querySelector(".section-hero");
+const header = document.querySelector('.header');
 
 const obs = new IntersectionObserver(
   function (entries) {
+    console.log(entries);
     const ent = entries[0];
     console.log(ent);
+    // console.log(ent);
 
     if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
+      return header.classList.add('sticky');
     }
 
     if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
+      return header.classList.remove('sticky');
     }
   },
   {
     // In the viewport
     root: null,
     threshold: 0,
-    rootMargin: "-80px",
+    rootMargin: '-80px',
   }
 );
-obs.observe(sectionHeroEl);
+obs.observe(document.querySelector('.section-hero'));
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  var flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
 
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
